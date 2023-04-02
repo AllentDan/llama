991f2fde7fc7422dfe5b7ab71dc7f983f096ef62
lvhan <lvhan_028@163.com>
2023-04-02 14:59:51 +0000

    update

--- communication/client.py
+++ communication/client.py
@@ -20,7 +20,7 @@ class Sender:
         request = inference_pb2.InferInput(prompts=message)
 
         # 发送请求到所有服务器
-        tasks = [await client.generate(request) for client in self.clents]
+        tasks = [client.generate(request) for client in self.clents]
         responses = await asyncio.gather(*tasks)
         print(f"{responses[0].text}")
 
@@ -44,8 +44,8 @@ async def send_request(server_address, message):
 
 
 async def main():
-    # client = Sender(['localhost:50051', 'localhost:50052'])
-
+    client = Sender(['localhost:50051', 'localhost:50052'])
+    await client.send_request('')
     # print('please input prompt:')
     # while True:
     #     print('>>')
@@ -54,11 +54,14 @@ async def main():
     #         await client.send_request(prompt)
     #     else:
     #         await client.close()
-    task1 = asyncio.create_task(send_request('localhost:50051', 'Hello from client 1'))
-    task2 = asyncio.create_task(send_request('localhost:50052', 'Hello from client 2'))
+    
+    
+    # the work version
+    # task1 = asyncio.create_task(send_request('localhost:50051', 'Hello from client 1'))
+    # task2 = asyncio.create_task(send_request('localhost:50052', 'Hello from client 2'))
 
-    # 等待所有任务完成
-    await asyncio.gather(task1, task2)
+    # # 等待所有任务完成
+    # await asyncio.gather(task1, task2)
 
 if __name__ == '__main__':
     asyncio.run(main())
